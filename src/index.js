182d229c8cd7b8c9d00a9f3ff718bb0a93f1b327
Korneel Grumieaux <99995736+KorneelGrumieaux@users.noreply.github.com>
2024-10-19 20:49:48 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import express from "express";
-import Boom from "@hapi/boom";
-import morgan from "morgan";
-import persistence from "./persistence/index.js";
+import express from 'express';
+import Boom from '@hapi/boom';
+import morgan from 'morgan';
+import persistence from './persistence/index.js';
 
 const PORT = 3000;
 
@@ -13,22 +13,22 @@ function asyncMiddleware(fn) {
 
 const app = express();
 
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(express.json());
 
 app.use((_, res, next) => {
-  res.set("X-Database-Used", process.env.MYSQL_URL ? "MySQL" : "SQLite");
+  res.set('X-Database-Used', process.env.MYSQL_URL ? 'MySQL' : 'SQLite');
   next();
 });
 
-app.get("/", (_, res) => {
+app.get('/', (_, res) => {
   res.send(
-    "It works, good job! You should try <code>/animals</code> or <code>/animals/:id</code>."
+    'It works, good job! You should try <code>/animals</code> or <code>/animals/:id</code>.'
   );
 });
 
 app.get(
-  "/animals",
+  '/animals',
   asyncMiddleware(async (_, res) => {
     const animals = await persistence.getAnimals();
     res.json(animals);
@@ -36,7 +36,7 @@ app.get(
 );
 
 app.get(
-  "/animals/:id",
+  '/animals/:id',
   asyncMiddleware(async (req, res) => {
     const animal = await persistence.getAnimal(Number(req.params.id));
     res.json(animal);
@@ -50,9 +50,9 @@ app.use((err, _, res, next) => {
   next();
 });
 
-console.log("test1");
-console.log("test2");
-console.log("test3");
+console.log('test1');
+console.log('test2');
+console.log('test3');
 persistence
   .initialize()
   .then(() => {
@@ -61,7 +61,7 @@ persistence
     });
   })
   .catch((err) => {
-    console.error("Database failed to connect, check the error below");
+    console.error('Database failed to connect, check the error below');
     console.error(err);
     process.exit(1);
   });
